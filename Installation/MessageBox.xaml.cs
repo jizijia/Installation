e3ccb6cb00ecba928107ad30ebebf810910aed42
jizijia <jizijia@qq.com>
2017-07-03 18:25:46 +0800

    增加提示窗口和定义样式

---
+++ Installation/MessageBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Installation
+{
+    /// <summary>
+    /// MessageBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class MessageWindow : Window
+    {
+        public MessageWindow(string title, string message)
+        {
+            InitializeComponent();
+            ImageBrush b = new ImageBrush();
+            b.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/MSGBack.png"));
+            b.Stretch = Stretch.Fill;
+            this.Background = b;
+        }
+
+        public static bool?  ShowMessage(string title, string message)
+        {
+            MessageWindow messageWindow = new MessageWindow(title, message);
+            return messageWindow.ShowDialog();
+        }
+    }
+}
